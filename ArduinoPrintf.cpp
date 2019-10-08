713778bbdc1f56e804abe0150bc752e29e53ae4b
Phillip Johnston <phillip@embeddedartistry.com>
2019-10-08 12:58:58 -0700

    Add printf library shims for Arduino

--- ArduinoPrintf.cpp
+++ ArduinoPrintf.cpp
@@ -1,1 +1,22 @@
 #include "ArduinoPrintf.h"
+#include "Arduino.h"
+#include <assert.h>
+
+static Print* print_instance = &Serial;
+
+void printf_init(Print* PrintClass)
+{
+	if(PrintClass)
+	{
+		print_instance = PrintClass;
+	}
+
+	assert(print_instance);
+}
+
+// If you use the default printf() implementation, this function will route the output
+// to the Serial class
+extern "C" __attribute__((weak)) void _putchar(char character)
+{
+	print_instance->print(character);
+}
--- ArduinoPrintf.h
+++ ArduinoPrintf.h
@@ -1,4 +1,11 @@
 #ifndef ARDUINO_PRINTF_H_
 #define ARDUINO_PRINTF_H_
 
+#include "Print.h"
+#include "printf.h"
+
+// In Setup(), you must initialize printf with a serial class.
+// You must also configure the Serial interface in setup() and call begin().
+void printf_init(Print* StreamClass = nullptr);
+
 #endif //ARDUINO_PRINTF_H_
