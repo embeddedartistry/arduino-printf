c4572f2c497e987d4106f8cf8a0022f12d2699d2
Phillip Johnston <phillip@embeddedartistry.com>
2020-05-26 11:44:37 -0700

    Auto-format library files

--- src/LibPrintf.cpp
+++ src/LibPrintf.cpp
@@ -19,7 +19,6 @@ void printf_init(Print& PrintClass)
 	print_instance = PrintClass;
 }
 
-
 // If you use the default printf() implementation, this function will route the output
 // to the Serial class
 extern "C" __attribute__((weak)) void _putchar(char character)
--- src/LibPrintf.h
+++ src/LibPrintf.h
@@ -1,8 +1,8 @@
 #ifndef ARDUINO_PRINTF_H_
 #define ARDUINO_PRINTF_H_
 
-#include "Print.h"
 #include "../extras/printf/printf.h"
+#include "Print.h"
 
 // In Setup(), you must initialize printf with a Print class if you don't want
 // to use the default Serial object. If you want the default behavior, calling this
@@ -12,4 +12,4 @@
 // Serial.begin().
 void printf_init(Print& StreamClass);
 
-#endif //ARDUINO_PRINTF_H_
+#endif // ARDUINO_PRINTF_H_
